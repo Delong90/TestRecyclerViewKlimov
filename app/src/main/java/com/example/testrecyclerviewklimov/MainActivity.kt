54cb94e4bf6461310f25fce8afaf65a6b70c8c7b
Delong90 <matushkin030790@gmail.com>
2021-06-15 20:05:35 +0300

    init

---
+++ app/src/main/java/com/example/testrecyclerviewklimov/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.testrecyclerviewklimov
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList())
+    }
+
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("$i element") }
+        return data
+    }
+
+    private fun getCatList(): List<String> {
+        return this.resources.getStringArray(R.array.cat_names).toList()
+    }
+}
